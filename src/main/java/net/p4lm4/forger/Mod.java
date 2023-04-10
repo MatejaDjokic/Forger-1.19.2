b5790d4b10557473d0d3d2bdeab555adb09d9ecb
_p4lm4 <matejadjokic23@gmail.com>
2023-04-10 18:09:55 +0200

    Initial commit

---
+++ src/main/java/net/p4lm4/forger/Mod.java
@@ -0,0 +1,36 @@
+package net.p4lm4.forger;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@net.minecraftforge.fml.common.Mod(Mod.MOD_ID)
+public class Mod {
+    public static final String MOD_ID = "forger";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public Mod() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        modEventBus.addListener(this::commonSetup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+
+    }
+
+    @net.minecraftforge.fml.common.Mod.EventBusSubscriber(modid = MOD_ID, bus = net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus.MOD)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+
+        }
+    }
+}
